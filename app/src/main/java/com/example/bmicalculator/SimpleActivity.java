84eeee7bdfbf7371fad4899cbe664161625d2cab
ThulaniThisarani <thisaraneethulani@gmail.com>
2025-02-12 15:47:37 +0530

    change button names

--- app/src/main/java/com/example/bmicalculator/SimpleActivity.java
+++ app/src/main/java/com/example/bmicalculator/SimpleActivity.java
@@ -1,7 +1,10 @@
 package com.example.bmicalculator;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -36,11 +39,14 @@ public class SimpleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_simple);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
         Button navigateButton=findViewById(R.id.food);
 
 
