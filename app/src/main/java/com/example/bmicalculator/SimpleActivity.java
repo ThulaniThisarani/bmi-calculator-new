ebb38ee86be462ac81333e21af69c9ede36fd9fd
ThulaniThisarani <thisaraneethulani@gmail.com>
2025-02-17 13:37:57 +0530

    Added new java feature and fixed issue

--- app/src/main/java/com/example/bmicalculator/SimpleActivity.java
+++ app/src/main/java/com/example/bmicalculator/SimpleActivity.java
@@ -46,6 +46,107 @@ public class SimpleActivity extends AppCompatActivity {
         });
 
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setElevation(0);
+            ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#1E1D1D"));
+            getSupportActionBar().setBackgroundDrawable(colorDrawable);
+            getSupportActionBar().setTitle(Html.fromHtml("<font color=\"white\">Result</font>"));
+        }
+
+
+        intent=getIntent();
+        mbmidisplay=findViewById(R.id.bmidisplay);
+        //    magedisplay=findViewById(R.id.agedisplay);
+        //    mweightdisplay=findViewById(R.id.weightdisplay);
+        mbmicategory = findViewById(R.id.bmicategory);
+//        mgotomain=findViewById(R.id.gotomain);
+
+        mimageview=findViewById(R.id.imageview);
+
+        //   mheightdisplay=findViewById(R.id.heightdisplay);
+        mgender=findViewById(R.id.genderdisplay);
+        mbackground=findViewById(R.id.contentlayout);
+        mrecalculatebmi=findViewById(R.id.recalculatebmi);
+
+
+        height=intent.getStringExtra("height");
+        weight=intent.getStringExtra("weight");
+
+
+        intheight=Float.parseFloat(height);
+        intweight=Float.parseFloat(weight);
+
+        intheight=intheight/100;
+        intbmi=intweight/(intheight*intheight);
+
+
+        mbmi=Float.toString(intbmi);
+//        System.out.println(mbmi);
+
+        if(intbmi<16)
+        {
+            mbmicategory.setText("Severe Thinness");
+            //   mbackground.setBackgroundColor(Color.GRAY);
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.cancel);
+            //  mimageview.setBackground(colorDrawable2);
+
+        }
+        else if(intbmi<16.9 && intbmi>16)
+        {
+            mbmicategory.setText("Moderate Thinness");
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.warning);
+            //   mimageview.setBackground(colorDrawable2);
+
+        }
+        else if(intbmi<18.4 && intbmi>17)
+        {
+            mbmicategory.setText("Mild Thinness");
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.warning);
+            //   mimageview.setBackground(colorDrawable2);
+        }
+        else if(intbmi<24.9 && intbmi>18.5 )
+        {
+            mbmicategory.setText("Normal");
+            mimageview.setImageResource(R.drawable.check);
+            mbackground.setBackgroundColor(Color.YELLOW);
+            //  mimageview.setBackground(colorDrawable2);
+        }
+        else if(intbmi <29.9 && intbmi>25)
+        {
+            mbmicategory.setText("Overweight");
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.warning);
+            //mimageview.setBackground(colorDrawable2);
+        }
+        else if(intbmi<34.9 && intbmi>30)
+        {
+            mbmicategory.setText("Obese Class I");
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.warning);
+            //  mimageview.setBackground(colorDrawable2);
+        }
+        else
+        {
+            mbmicategory.setText("Obese Class II");
+            mbackground.setBackgroundColor(Color.RED);
+            mimageview.setImageResource(R.drawable.cancel);
+            //  mimageview.setBackground(colorDrawable2);
+        }
+
+        //magedisplay.setText("your age is"+intent.getStringExtra("age"));
+        //mheightdisplay.setText("Your Height is "+intent.getStringExtra("height"));
+        //mweightdisplay.setText("Your Weight is "+intent.getStringExtra("weight"));
+        mgender.setText(intent.getStringExtra("gender"));
+        mbmidisplay.setText(mbmi);
+
+
+
+
+
+
 
         Button navigateButton=findViewById(R.id.food);
 
